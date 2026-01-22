9a7b601e612f976e35ed69c5f8afa0b754aadce6
sejalpiwal97-collab <sejalpiwal97@gmail.com>
2026-01-22 23:16:11 +0530

    02_datatypes.js

---
+++ 02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3)//we are using nodejs, not browser
+//code readibiliy should be high
+//ECMAScript for documentation extra learnig
+console.log("sejal")
+
+let name =  "sejal"
+let age = 22
+let isLoggedIn = false
+let state; ""
+
+
+//number => 2 to power of 53
+//bigInt
+//string =""
+//booleam => true/false
+//null => stand alone value
+//undefined => value not assigned yet
+// symobol =>
+
+    //object 
+
+console.log(typeof undefined )    
---
+++ o1_variables.js
@@ -0,0 +1,20 @@
+const accountId =949704
+let accountEmail = "sejalpiwal97@gmail.com"
+var accoutPassword = "12345"
+accountCity = "Nalasopara"
+let accountState;
+ 
+
+//accountId = 2 //
+/*
+Prefer not to use var
+bacause of issue in block scope nd functional  scope 
+*/
+
+accountEmail = "hc@hc.com"
+accoutPassword = "662"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table ([ accountId, accountEmail, accoutPassword , accountCity, accountState])
