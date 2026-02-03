06dcb6807b9dd7c6f6823c3ab5aefc3ca24fa03a
sejalpiwal97-collab <sejalpiwal97@gmail.com>
2026-02-03 22:24:51 +0530

    o1_variables.js

---
+++ 03_conOrOper.js
@@ -0,0 +1,56 @@
+//let score = "33abc"
+
+//console.log( typeof score);
+///console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33
+// "33ac" => NaN 
+// true => 1; false => 0
+
+//let isLoggedIn  = 1
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// ********************operation********************
+//let value = 3
+//let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2%2);
+
+//let str1 = "hello"
+//let str2 =" sejal"
+
+//let str3 = str1 + str2
+//console.log(str3);
+
+//console.log( "1" + 2);
+//console.log (1 + "2");
+//console.log ("1" + 2 + 2);
+//console.log (1 + 2 + "2");
+
+//console.log( (3 + 4) *5 % 3)
+
+//console.log(true);
+
+//let num1, num2, num3
+
+//num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+gameCounter++;
+console.log(gameCounter);
+
+//mdn documents
+
