d8c454a7a1081ea46a67a47df80b9de3cd283cb0
sejalpiwal97-collab <sejalpiwal97@gmail.com>
2026-02-04 15:46:15 +0530

    o1_variables.js

---
+++ datatype_summary.js
@@ -0,0 +1,24 @@
+//# Primitive
+
+//7 types : String , number, boolean, null, undefined, symbol, BigInt
+
+const score =  100
+
+const scoreValue =  100.2
+const isLoggedIn = false 
+
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 689979649469352n
+
+
+
+//Reference (Non Primitive )
+
+//Array , objects, functions
